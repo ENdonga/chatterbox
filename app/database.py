917d29d64bc21924626ca608f877ac038433bc86
ndonga <ndonga.edwin@gmail.com>
2025-02-26 09:04:04 +0300

    load database config from .env file with pydantic

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'postgresql://postgres:postgres@localhost/chatterbox_db'
+from app.config import settings
+
+DATABASE_URL = settings.database_url
 
 engine = create_engine(DATABASE_URL)
 
